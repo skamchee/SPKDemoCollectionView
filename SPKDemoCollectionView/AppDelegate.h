07f9ef45c144c7cb8c5b3f9b67262bd4210fda0a
Spencer Kamchee <s.kamchee@gmail.com>
2016-02-24 20:35:50 -0600

    Initial Commit

---
+++ SPKDemoCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SPKDemoCollectionView
+//
+//  Created by Spencer Kamchee on 2/24/16.
+//  Copyright © 2016 Spencer Kamchee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SPKDemoCollectionView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SPKDemoCollectionView
+//
+//  Created by Spencer Kamchee on 2/24/16.
+//  Copyright © 2016 Spencer Kamchee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
